062970b2394dbac866c4dfe8f3d6a72e7d11c85b
MagmaGuy <tiagoarnaut@gmail.com>
2026-06-18 20:43:03 +0100

    Fix Bedrock vanilla item scanning and relay polling

--- resourcepackmanager-bedrock/src/test/java/com/magmaguy/resourcepackmanager/bedrock/generic/GenericJavaScannerTest.java
+++ resourcepackmanager-bedrock/src/test/java/com/magmaguy/resourcepackmanager/bedrock/generic/GenericJavaScannerTest.java
@@ -17,6 +17,50 @@ class GenericJavaScannerTest {
     Path tempDir;
 
     @Test
+    void modernMinecraftItemsDefinitionWithStringCustomModelDataIsScanned() throws Exception {
+        Path vanillaItemDefinition = tempDir.resolve("assets/minecraft/items/carrot_on_a_stick.json");
+        Files.createDirectories(vanillaItemDefinition.getParent());
+        Files.writeString(vanillaItemDefinition, """
+                {
+                  "model": {
+                    "type": "minecraft:select",
+                    "property": "minecraft:custom_model_data",
+                    "cases": [
+                      {
+                        "when": "greenshell",
+                        "model": {
+                          "type": "minecraft:model",
+                          "model": "item/greenshell"
+                        }
+                      }
+                    ]
+                  }
+                }
+                """);
+
+        List<ItemsDefinition> definitions = GenericJavaScanner.scan(tempDir.toFile());
+
+        assertEquals(1, definitions.size());
+        ItemsDefinition def = definitions.get(0);
+        assertEquals("minecraft", def.namespace());
+        assertEquals("carrot_on_a_stick", def.itemsRelPath());
+        assertEquals("minecraft:carrot_on_a_stick", def.itemIdentifier());
+        assertEquals(List.of("minecraft:carrot_on_a_stick"),
+                BaseItemResolver.resolve(def, new AssetResolver(tempDir.toFile())));
+
+        List<ResolvedLeaf> leaves = ItemModelTreeWalker.walk(def, null);
+        assertEquals(1, leaves.size());
+        assertEquals("item/greenshell", leaves.get(0).modelRef());
+        assertEquals(1, leaves.get(0).predicates().size());
+
+        JsonObject predicate = leaves.get(0).predicates().get(0).toGeyserJson();
+        assertEquals("match", predicate.get("type").getAsString());
+        assertEquals("custom_model_data", predicate.get("property").getAsString());
+        assertEquals("greenshell", predicate.get("value").getAsString());
+        assertEquals(0, predicate.get("index").getAsInt());
+    }
+
+    @Test
     void legacyCustomModelDataOverrideProducesDefinitionWithExplicitBaseItem() throws Exception {
         Path vanillaItemModel = tempDir.resolve("assets/minecraft/models/item/paper.json");
         Files.createDirectories(vanillaItemModel.getParent());
